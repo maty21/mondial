e8bee85076cab5db45a5b59de5874876e4c79ba8
yehiyam <yehiyam@gmail.com>
2018-06-17 08:29:52 +0300

    add deploy

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo Deploy to kubernetes
-source ./setPath
-hkube deploy --cluster_name=pub -c
-until $(curl -k --output /dev/null --silent --head --fail https://${KUBERNETES_MASTER_IP}/hkube/api-server/api/); do
-    printf '.'
-    sleep 5
-done
